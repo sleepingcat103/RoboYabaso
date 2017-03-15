c3c0c659590b153e20b4e3086c391df1fd1e436e
sleepingcat103 <sleepingcat103@gmail.com>
2017-03-15 15:42:26 +0800

    Create 00.js

---
+++ 00.js
@@ -0,0 +1,237 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var https = require('https');  
+var app = express();
+
+var jsonParser = bodyParser.json();
+
+var options = {
+  host: 'api.line.me',
+  port: 443,
+  path: '/v2/bot/message/reply',
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer actVI2pGSgmQ+JYuF2il02qMYH+1+3Q6pvaTjjL4J77uWSuVRoTZnloLqZG39jxfuZAWyS77LfHuQ9rHx4vupzxq3sDLKcwRraRq0F0t9B8aULHlhuO2BYmiIvOFjT6Vs+RFkd3GDQnNB2Ykvo6rlgdB04t89/1O/w1cDnyilFU='    
+  }
+}
+app.set('port', (process.env.PORT || 5000));
+
+// views is directory for all template files
+
+app.get('/', function(req, res) {
+//  res.send(parseInput(req.query.input));
+  res.send('Hello');
+});
+
+app.post('/', jsonParser, function(req, res) {
+  let event = req.body.events[0];
+  let type = event.type;
+  let msgType = event.message.type;
+  let msg = event.message.text;
+  let rplyToken = event.replyToken;
+
+  let rplyVal = null;
+  console.log(msg);
+  if (type == 'message' && msgType == 'text') {
+    try {
+      rplyVal = parseInput(rplyToken, msg); 
+    } 
+    catch(e) {
+      //rplyVal = randomReply();
+      console.log('總之先隨便擺個跑到這邊的訊息，catch error');
+    }
+  }
+
+  if (rplyVal) {
+    replyMsgToLine(rplyToken, rplyVal); 
+  } else {
+    console.log('Do not trigger'); 
+  }
+
+  res.send('ok');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+function replyMsgToLine(rplyToken, rplyVal) {
+  let rplyObj = {
+    replyToken: rplyToken,
+    messages: [
+      {
+        type: "text",
+        text: rplyVal
+      }
+    ]
+  }
+
+  let rplyJson = JSON.stringify(rplyObj); 
+  
+  var request = https.request(options, function(response) {
+    console.log('Status: ' + response.statusCode);
+    console.log('Headers: ' + JSON.stringify(response.headers));
+    response.setEncoding('utf8');
+    response.on('data', function(body) {
+      console.log(body); 
+    });
+  });
+  request.on('error', function(e) {
+    console.log('Request error: ' + e.message);
+  })
+  request.end(rplyJson);
+}
+
+function parseInput(rplyToken, inputStr) {
+        console.log('InputStr: ' + inputStr);
+        _isNaN = function(obj) {
+         return isNaN(parseInt(obj));
+        }                   
+        //ROBOT START
+
+        if (inputStr.match('貓咪') != null) return YabasoReply(inputStr) ;
+		
+        //cc判定在此
+        //elseif (inputStr.toLowerCase().match(/^cc/)!= null) return CoC7th(inputStr.toLowerCase()) ;      
+        
+		//擲骰判定在此        
+        else if (inputStr.match(/\w/)!=null && inputStr.toLowerCase().match(/d/)!=null) {
+          return nomalDiceRoller(inputStr);
+        }
+  
+        
+        else return undefined;
+        
+      }
+
+
+        
+function nomalDiceRoller(inputStr){
+  
+  //先定義要輸出的Str
+  let finalStr = '' ;  
+ //首先判斷是否是誤啟動（檢查是否有符合骰子格式）
+  if (inputStr.toLowerCase().match(/\d+d\d+/) == null) return undefined;
+
+  //再來先把第一個分段拆出來，待會判斷是否是複數擲骰
+  let mutiOrNot = inputStr.toLowerCase().match(/\S+/);
+
+  //排除小數點
+  if (mutiOrNot.toString().match(/\./)!=null)return undefined;
+
+  if(mutiOrNot.toString().match(/\D/)==null )  {
+    finalStr= '複數擲骰：'
+    if(mutiOrNot>20) return '不支援20次以上的複數擲骰。';
+
+    for (i=1 ; i<=mutiOrNot ;i++){
+      let DiceToRoll = inputStr.toLowerCase().split(' ',2)[1];
+      if (DiceToRoll.match('d') == null) return undefined;
+      finalStr = finalStr +'\n' + i + '# ' + DiceCal(DiceToRoll);
+    }
+    if(finalStr.match('200D')!= null) finalStr = '不支援200D以上擲骰唷';
+    if(finalStr.match('D500')!= null) finalStr = '不支援D1和超過D500的擲骰唷';
+    
+  } 
+  
+  else finalStr= '基本擲骰：' + DiceCal(mutiOrNot.toString());
+  
+  if (finalStr.match('NaN')!= null||finalStr.match('undefined')!= null) return undefined;
+  return finalStr;
+}
+        
+//作計算的函數
+function DiceCal(inputStr){
+  
+  //首先判斷是否是誤啟動（檢查是否有符合骰子格式）
+  if (inputStr.toLowerCase().match(/\d+d\d+/) == null) return undefined;
+    
+  //排除小數點
+  if (inputStr.toString().match(/\./)!=null)return undefined;
+
+  //先定義要輸出的Str
+  let finalStr = '' ;  
+  
+  //一般單次擲骰
+  let DiceToRoll = inputStr.toString().toLowerCase();  
+  if (DiceToRoll.match('d') == null) return undefined;
+  
+  //寫出算式
+  let equation = DiceToRoll;
+  while(equation.match(/\d+d\d+/)!=null) {
+    let tempMatch = equation.match(/\d+d\d+/);    
+    if (tempMatch.toString().split('d')[0]>200) return '不支援200D以上擲骰唷';
+    if (tempMatch.toString().split('d')[1]==1 || tempMatch.toString().split('d')[1]>500) return '不支援D1和超過D500的擲骰唷';
+    equation = equation.replace(/\d+d\d+/, RollDice(tempMatch));
+  }
+  
+  //計算算式
+  let answer = eval(equation.toString());
+    finalStr= equation + ' = ' + answer;
+  
+  return finalStr;
+
+
+}        
+
+//用來把d給展開成算式的函數
+function RollDice(inputStr){
+  //先把inputStr變成字串（不知道為什麼非這樣不可）
+  let comStr=inputStr.toString().toLowerCase();
+  let finalStr = '(';
+
+  for (let i = 1; i <= comStr.split('d')[0]; i++) {
+    finalStr = finalStr + Dice(comStr.split('d')[1]) + '+';
+     }
+
+  finalStr = finalStr.substring(0, finalStr.length - 1) + ')';
+  return finalStr;
+}
+                                                                     
+      
+
+function Dice(diceSided){          
+          return Math.floor((Math.random() * diceSided) + 1)
+        }              
+
+
+function YabasoReply(inputStr) { 
+  //一般功能說明
+  if (inputStr.match('說明') != null) return YabasoReply('0') + '\
+\n \
+\n支援\
+\n四則運算：2d4+1、2D10+1d2\
+\n多筆輸出，先打你要的次數，再空一格打骰數：7 3d6、5 2d6+6  \
+\n大小寫不拘 \
+\n \
+\n運勢：你只要提到我的名字和運勢，我就會回答你的運勢。 \
+\n隨機選擇：只要提到我的名字和[選、挑、隨機]，然後空一格打選項。 \
+\n \
+\n鴨霸獸的簡化版，感謝yabaso\
+';
+  else
+	
+  //貓咪幫我選～～
+  if(inputStr.match('選') != null||inputStr.match('隨機') != null||inputStr.match('挑') != null) {
+    let rplyArr = inputStr.split(' ');
+    
+    if (rplyArr.length == 1) return '一個選項是要選毛喔，貓咪鄙視';
+    
+    let Answer = rplyArr[Math.floor((Math.random() * (rplyArr.length-1))+ 1)];
+
+    return '恩......，' + Answer + '好了！';
+  }
+  else
+    
+  //以下是運勢功能
+  if(inputStr.match('運勢') != null){
+    let rplyArr=['超大吉','大吉','大吉','中吉','中吉','中吉','小吉','小吉','小吉','小吉','凶','凶','凶','大凶','大凶','你還是，不要知道比較好','這應該不關我的事'];
+    return '運勢喔…我覺得，' + rplyArr[Math.floor((Math.random() * (rplyArr.length)) + 0)] + '吧。';
+  } 
+  
+  //沒有觸發關鍵字則是這個
+  else{
+	return '要做什麼喵？';
+  }
+
+}
