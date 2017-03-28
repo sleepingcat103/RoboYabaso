c850c25658ba296f0bff3c879ac4eb80e99f9bcf
sleepingcat103 <sleepingcat103@gmail.com>
2017-03-28 23:58:31 +0800

    Update index.js

--- index.js
+++ index.js
@@ -254,7 +254,7 @@ function CharacterControll(trigger, str1, str2, str3){
 		
 		for(i=0; i<5; i++) {
 			if(players[i].getVal('name') == '') {
-				players[i].set('name', str1);
+				players[i].set('name', str1, '');
 				return '成功建立角色 ' + str1 + ' 請補充他/她的能力值!';
 			}
 		}
