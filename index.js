714116d42b479b7f8aea11b8d4c2fd3d56f8e3df
IvreaHuang <eunice82729@gmail.com>
2017-04-06 15:18:52 +0800

    Update index.js

--- index.js
+++ index.js
@@ -385,8 +385,7 @@ function CharacterControll(trigger, str1, str2) {
         for (i = 0; i < players.length; i++) {
             if (players[i].name == str1) return '已經有同名的角色了!';
         }
-	var player = new Player(str1);
-	players.push(player);
+	players.push(new Player(str1));
 	return '成功建立角色 ' + player.getVal('name') + ' 請補充他/她的能力值!'
     }
 
