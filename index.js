756664a1a0b6c6c54fc7138b3907890e61d9f7c8
IvreaHuang <eunice82729@gmail.com>
2017-04-08 00:06:15 +0800

    Update index.js

--- index.js
+++ index.js
@@ -239,13 +239,13 @@ function createChar(p_name,p_uid){
      player.importFromTRPG = function(p_str){
 	var tempChar = JSON.parse(p_str);
 	var newChar = tempChar.skill;
-	//for (var p in newChar){
-	//   if(this.status.hasOwnProperty(JSONmapping[p])){
-	//	eval('this.setVal(\''+ JSONmapping[p] +'\',\''+ newChar[p] +'\')'  );
-	//   }
-	//}
-	//return '成功匯入角色 ' + this.getVal('name') + ' !!!!';
-	return JSON.stringify(newChar);
+	for (var p in newChar){
+	   if(this.status.hasOwnProperty(JSONmapping[p])){
+		eval('this.status.'+ JSONmapping[p] +'=\''+ newChar[p] +'\'');
+	   }
+	}
+	return '成功匯入角色 ' + this.getVal('name') + ' !!!!';
+	//return JSON.stringify(newChar);
      };
      return player;
 }
