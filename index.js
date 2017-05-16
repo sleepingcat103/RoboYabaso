798a64bcf8926380ea3bf0373ad0ce650079758c
sleepingcat103 <sleepingcat103@gmail.com>
2017-05-16 14:45:58 +0800

    Update index.js

--- index.js
+++ index.js
@@ -133,6 +133,33 @@ function replyMsgToLine(outType, rplyToken, rplyVal) {
               }
             ]
         }//
+    } else if (outType == 'template') {
+	v_path = '/v2/bot/message/reply';
+        rplyObj = {
+	    replyToken: rplyToken,
+	    messages: [
+		{
+		  "type": "template",
+		  "altText": "this is a buttons template",
+		  "template": {
+		      "type": "buttons",
+		      "text": "Please select",
+		      "actions": [
+			  {
+			    "type": "message",
+			    "label": "test 1d6",
+			    "text": "zz"
+			  },
+			  {
+			    "type": "message",
+			    "label": "message",
+			    "text": "return message"
+			  }
+		      ]
+		  }
+		}
+            ]
+        }
     } else {
         v_path = '/v2/bot/message/reply';
         rplyObj = {
@@ -537,6 +564,8 @@ function parseInput(roomMID,rplyToken, inputStr) {
 	       userToRoom[event.source.userId].userId + '\n'+
 	       userToRoom[event.source.userId].pictureUrl + '\n'+
 	       userToRoom[event.source.userId].statusMessage;
+    }else if(trigger == 'template'){
+	replyMsgToLine('template', rplyToken,'');    
     }
 }
 
