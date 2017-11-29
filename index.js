b54efed1676f6c1144827dcc93ccef7ca69cc09c
sleepingcat103 <sleepingcat103@gmail.com>
2017-11-29 19:22:27 +0800

    Update index.js

--- index.js
+++ index.js
@@ -609,8 +609,25 @@ https://raw.githubusercontent.com/sleepingcat103/RoboYabaso/master/lc-0.jpg'];
         let s = inputStr.replace('voice ', '');
 	outType = 'audio';
 	voicelength = s.length*300;
-
-	return 'https://api.cloudconvert.com/convert?apikey=u79J7MEFzGBGi_5TBUc6OyqgMNaX9mo6pV7hRsF6p4RHsHzmOFfCDxazShtLxUeM2KAQVbjd2PC4zyzG2syr-A&inputformat=mp3&outputformat=m4a&input=download&file=https%3A%2F%2Fapi.voicerss.org%2F%3Fkey%3Dad9bb556e281481093e10b10ffc673e5%26hl%3Den-us%26src%3D'+ encodeURIComponent(s)+'%26c%3Dmp3&filename=file.mp3&wait=true&download=inline'; 
+ 	s = 'https://api.voicerss.org/?key=ad9bb556e281481093e10b10ffc673e5&hl=en-us&src='+ encodeURIComponent(s)+'&c=aac';
+	    
+	$.ajax({
+	    url : "https://www.googleapis.com/urlshortener/v1/url",
+	    type : "POST",
+	    data : JSON.stringify({
+	        longUrl: s
+	    }),
+	    contentType : "application/json",
+	    dataType : "json",
+	    success: function(result,status,xhr){
+	        if ( status=="success" )
+	        {
+	            //取得的 result.id 即為轉換後的 短網址
+		    console.log(result.id);
+		    return result.id;
+	        }
+	    }
+	});
 	//return 'https://api.voicerss.org/?key=ad9bb556e281481093e10b10ffc673e5&hl=en-us&src='+ encodeURIComponent(s)+'&c=aac';
     }
 }
