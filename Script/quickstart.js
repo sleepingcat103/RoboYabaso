eee6b984ac962a5db2a2d35a1c97e472792e810a
sleepingcat103 <sleepingcat103@gmail.com>
2018-01-25 17:41:46 +0800

    Update quickstart.js

--- Script/quickstart.js
+++ Script/quickstart.js
@@ -1,124 +1,68 @@
-var fs = require('fs');
-var readline = require('readline');
+
 var google = require('googleapis');
 var googleAuth = require('google-auth-library');
+var express = require('express');
 
-// If modifying these scopes, delete your previously saved credentials
-// at ~./sheetsapi.json
-var SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
-var TOKEN_DIR = './';
-var TOKEN_PATH = TOKEN_DIR + 'sheetsapi.json';
+//google sheet
+//client_secret.json
+var myClientSecret = {"installed":{"client_id":"679508808367-6foqlrkagv2v7qusqua0nkmvo6b424c6.apps.googleusercontent.com","project_id":"my-project-1511956147962","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://accounts.google.com/o/oauth2/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"1-w0sdtE0LT7ZCWrdVUIdwNy","redirect_uris":["urn:ietf:wg:oauth:2.0:oob","http://localhost"]}};
 
-// Load client secrets from a local file.
-fs.readFile('client_secret.json', function processClientSecrets(err, content) {
-  if (err) {
-    console.log('Error loading client secret file: ' + err);
-    return;
-  }
-  // Authorize a client with the loaded credentials, then call the
-  // Google Sheets API.
-  authorize(JSON.parse(content), listMajors);
-});
+var auth = new googleAuth();
+var oauth2Client = new auth.OAuth2(myClientSecret.installed.client_id,myClientSecret.installed.client_secret, myClientSecret.installed.redirect_uris[0]);
 
-/**
- * Create an OAuth2 client with the given credentials, and then execute the
- * given callback function.
- *
- * @param {Object} credentials The authorization client credentials.
- * @param {function} callback The callback to call with the authorized client.
- */
-function authorize(credentials, callback) {
-  var clientSecret = credentials.installed.client_secret;
-  var clientId = credentials.installed.client_id;
-  var redirectUrl = credentials.installed.redirect_uris[0];
-  var auth = new googleAuth();
-  var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
+//sheetsapi.json
+oauth2Client.credentials = {"access_token":"ya29.GltNBUPTeXr094wOq6KSEJqYg4DXtkLyvs8hAdA3lDc4Zc6a8GLj3vd69ZAWHy7M7egwIgPpw2LJO9l432puFcErwTfwmp5S3eClJudQi7OBknYVEsKmVjvAqGEx","refresh_token":"1/oZUfFdL0n5tpgs5JPTcQQCCtpI3D-Tyn4CFmFZNfyfI","token_type":"Bearer","expiry_date":1516871850908};
 
-  // Check if we have previously stored a token.
-  fs.readFile(TOKEN_PATH, function(err, token) {
-    if (err) {
-      getNewToken(oauth2Client, callback);
-    } else {
-      oauth2Client.credentials = JSON.parse(token);
-      callback(oauth2Client);
-    }
-  });
-}
+//試算表的ID
+var mySheetId='1QvtxfT4PXrIXwC-gbWABddmrwhd0-zaU4JyNRuHR-ig';
 
-/**
- * Get and store new token after prompting for user authorization, and then
- * execute the given callback with the authorized OAuth2 client.
- *
- * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
- * @param {getEventsCallback} callback The callback to call with the authorized
- *     client.
- */
-function getNewToken(oauth2Client, callback) {
-  var authUrl = oauth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: SCOPES
-  });
-  console.log('Authorize this app by visiting this url: ', authUrl);
-  var rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-  rl.question('Enter the code from that page here: ', function(code) {
-    rl.close();
-    oauth2Client.getToken(code, function(err, token) {
-      if (err) {
-        console.log('Error while trying to retrieve access token', err);
-        return;
-      }
-      oauth2Client.credentials = token;
-      storeToken(token);
-      callback(oauth2Client);
+var sheets = google.sheets('v4');
+
+update([["Door", "$15", "2", "3/15/2016"],["Engine", "$100", "1", "3/20/2016"]]);
+
+function getSheetContent() {
+    sheets.spreadsheets.values.get({
+        auth: oauth2Client,
+        spreadsheetId: mySheetId,
+        range:encodeURI('test'),
+    }, function(err, response) {
+        if (err) {
+            console.log('Error: '+err.message);
+            return;
+        }else{
+			//console.log('success!');
+		}
+        var rows = response.values;
+        console.log(rows);
     });
-  });
 }
 
-/**
- * Store token to disk be used in later program executions.
- *
- * @param {Object} token The token to store to disk.
- */
-function storeToken(token) {
-  try {
-    fs.mkdirSync(TOKEN_DIR);
-  } catch (err) {
-    if (err.code != 'EEXIST') {
-      throw err;
-    }
-  }
-  fs.writeFile(TOKEN_PATH, JSON.stringify(token));
-  console.log('Token stored to ' + TOKEN_PATH);
+function clearSheet(){
+	sheets.spreadsheets.values.clear({
+        auth: oauth2Client,
+        spreadsheetId: mySheetId,
+        range:encodeURI('test'),
+    }, function(err, response) {
+        if (err) {
+            console.log('Error: '+err.message);
+            return;
+        }else{console.log('success!');}
+    });
 }
 
-/**
- * Print the names and majors of students in a sample spreadsheet:
- * https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
- */
-function listMajors(auth) {
-  var sheets = google.sheets('v4');
-  sheets.spreadsheets.values.get({
-    auth: auth,
-    spreadsheetId: '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms',
-    range: 'Class Data!A2:E',
-  }, function(err, response) {
-    if (err) {
-      console.log('The API returned an error: ' + err);
-      return;
-    }
-    var rows = response.values;
-    if (rows.length == 0) {
-      console.log('No data found.');
-    } else {
-      console.log('Name, Major:');
-      for (var i = 0; i < rows.length; i++) {
-        var row = rows[i];
-        // Print columns A and E, which correspond to indices 0 and 4.
-        console.log('%s, %s', row[0], row[4]);
-      }
-    }
-  });
+function update(data) {
+    sheets.spreadsheets.values.update({
+	    spreadsheetId: mySheetId,
+		range: 'test',
+		valueInputOption: "RAW",
+		auth: oauth2Client,
+		resource: {
+			"values": {data}
+		},
+    }, function(err, response) {
+        if (err) {
+            console.log('Error: '+err.message);
+            return;
+        }else{console.log('success!');}
+    });
 }
