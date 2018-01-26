06dccc1df7d32a986ff02513c57680175027cf21
sleepingcat103 <sleepingcat103@gmail.com>
2018-01-26 15:29:08 +0800

    Update test.js

--- Script/test.js
+++ Script/test.js
@@ -1,7 +1,7 @@
-
 var google = require('googleapis');
 var googleAuth = require('google-auth-library');
-var express = require('express');
+var request = require('request');
+var sheetrock = require('sheetrock');
 
 //google sheet
 //client_secret.json
@@ -15,13 +15,27 @@ oauth2Client.credentials = {"access_token":"ya29.GltNBUPTeXr094wOq6KSEJqYg4DXtkL
 
 //試算表的ID
 var mySheetId='1QvtxfT4PXrIXwC-gbWABddmrwhd0-zaU4JyNRuHR-ig';
-
 var sheets = google.sheets('v4');
 
-update([["Door", "$15", "2", "3/15/2016"],["Engine", "$100", "1", "3/20/2016"]]);
 
-function getSheetContent() {
-    sheets.spreadsheets.values.get({
+
+//測試
+try {
+	
+	//Player Format
+	var player = [["4","roomid", "playerid", "CharacterName", "StatusJSon"]];
+	
+	SheetDelete(player);
+	//SheetEdit(player);
+	
+}catch(e){
+	console.log(e);
+}
+
+//整理表格 (刪除空白行)
+function SheetRefresh(){
+	
+	sheets.spreadsheets.values.get({
         auth: oauth2Client,
         spreadsheetId: mySheetId,
         range:encodeURI('test'),
@@ -30,39 +44,130 @@ function getSheetContent() {
             console.log('Error: '+err.message);
             return;
         }else{
-			//console.log('success!');
+			var rows = response.values;
+			
+			 //(取出有資料的) 
+			var playerList = rows.filter(function (element,index,array) {
+				if(element[1]!=null){
+					return element ;
+				}
+			});
+			
+			sheets.spreadsheets.values.clear({
+				auth: oauth2Client,
+				spreadsheetId: mySheetId,
+				range:encodeURI('test'),
+			}, function(err, response) {
+				if (err) {
+					console.log('Error: '+err.message);
+					return;
+				}else{
+					SheetAdd(playerList);  //加回去
+				}
+			});
 		}
-        var rows = response.values;
-        console.log(rows);
     });
 }
 
-function clearSheet(){
-	sheets.spreadsheets.values.clear({
-        auth: oauth2Client,
-        spreadsheetId: mySheetId,
-        range:encodeURI('test'),
-    }, function(err, response) {
-        if (err) {
-            console.log('Error: '+err.message);
-            return;
-        }else{console.log('success!');}
-    });
-}
-
-function update(data) {
+function SheetAdd(player) {
+	//檢查重複
+	var selectQuery = 'select A,B,C,D,E WHERE D = ' + player[0][3] + ' and A = ' + player[0][1];
+	sheetrock({
+		url: 'https://docs.google.com/spreadsheets/d/1QvtxfT4PXrIXwC-gbWABddmrwhd0-zaU4JyNRuHR-ig/edit#gid=0',
+		query: selectQuery,
+		callback: function (error, options, response) {
+			if(error) {
+				console.log('Fail to get data: ' +　error);
+			} else {
+				if(response.rows.length>1){
+					console.log('Character already Existed!');
+					return;
+				}
+			}
+		}
+	});
+	
     sheets.spreadsheets.values.update({
 	    spreadsheetId: mySheetId,
 		range: 'test',
 		valueInputOption: "RAW",
 		auth: oauth2Client,
 		resource: {
-			"values": {data}
+			"values": player
 		},
     }, function(err, response) {
         if (err) {
             console.log('Error: '+err.message);
             return;
-        }else{console.log('success!');}
+        }else{console.log('Add success!');}
     });
 }
+
+//single Edit
+function SheetEdit(player) {
+	var No = player[0][0];
+    sheets.spreadsheets.values.update({
+	    spreadsheetId: mySheetId,
+		range: 'test!A' + No + ':E' + No,
+		valueInputOption: "RAW",
+		auth: oauth2Client,
+		resource: {
+			"values": player
+		},
+    }, function(err, response) {
+        if (err) {
+            console.log('Error: '+err.message);
+            return;
+        }else{console.log('Edit success!');}
+    });
+}
+
+//single Delete
+function SheetDelete(player) {
+	var No = player[0][0];
+	player = [[No,"","","",""]];
+	
+    sheets.spreadsheets.values.update({
+	    spreadsheetId: mySheetId,
+		range: 'test!A' + No + ':E' + No,
+		valueInputOption: "RAW",
+		auth: oauth2Client,
+		resource: {
+			"values": player
+		},
+    }, function(err, response) {
+        if (err) {
+            console.log('Error: '+err.message);
+            return;
+        }else{console.log('Edit success!');}
+    });
+}
+
+//抓取房間玩家&KP資訊
+function GetDataByRoomId(roomID){
+	var selectQuery = 'select A,B,C,D,E WHERE B = ' + roomID;
+	sheetrock({
+		url: 'https://docs.google.com/spreadsheets/d/1QvtxfT4PXrIXwC-gbWABddmrwhd0-zaU4JyNRuHR-ig/edit#gid=0',
+		query: selectQuery,
+		callback: function (error, options, response) {
+			if(error) {
+				console.log('Fail to get data: ' +　error);
+			} else {
+				if(response.rows.length>1){
+					//製作房間&角色資訊
+					console.log(response.rows[1].cellsArray);
+				}
+			}
+		}
+	});
+}
+
+function GenerateGoogleSheetData(player){
+	//line bot 紀錄格式 轉 google Sheet 儲存模式
+	//[["No","roomid", "playerid", "StatusJSon", "IsKP"]];
+}
+
+function GenerateLineBotData(player){
+	//google Sheet 儲存模式 轉 line bot 紀錄格式 
+}
+
