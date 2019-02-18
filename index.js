7723cc38e7b9d14779271816fb6024b3c29a7c36
sleepingcat103 <sleepingcat103@gmail.com>
2019-02-18 22:47:01 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1415,7 +1415,7 @@ function gotcha(){
     if(result.join('') == 'RRRRRRRRRR') {
         result[9] = 'SR';
     }
-    if((result.match(/SS/g) || []).length == 0 && (result.match(/S/g) || []).length < 3) {
+    if((result.join('').match(/SS/g) || []).length == 0 && (result.join('').match(/S/g) || []).length < 3) {
 	rplyArr = ['保底 ㄏ', '可撥的非洲仔 z', '非洲歡迎你，朋友', '再...再10抽一定出', '....跟下個月的借錢好了', '下次會更好...吧?', '123 出彩好幾單'];
 	msg = rplyArr[Math.floor((Math.random() * (rplyArr.length)) + 0)];
     }
